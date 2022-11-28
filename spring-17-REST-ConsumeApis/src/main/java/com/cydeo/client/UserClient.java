cdcd72896e6c0517b37b2aba39323c6ac2c82e7c
DilaraAsl <dcavus@gmail.com>
2022-11-28 11:43:27 -0800

    new files added

---
+++ spring-17-REST-ConsumeApis/src/main/java/com/cydeo/client/UserClient.java
@@ -0,0 +1,13 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+//https://jsonplaceholder.typicode.com/users we are consuming this url
+@FeignClient(url = "https://jsonplaceholder.typicode.com",name="USER-CLIENT") // any name can be given in microservices naming is important
+public interface UserClient {
+    @GetMapping("/users")
+    List<User> getUsers();// whenever we call this method anywhere in the application it will hit the end point above the end point will be executed with @GetMapping
+}
