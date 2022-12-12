499f00f3547fa0fb679a66108f0b03b6c08846b9
DilaraAsl <dcavus@gmail.com>
2022-12-11 19:07:51 -0800

    committing module 21

--- spring-17-REST-ConsumeApis/src/main/java/com/cydeo/Spring17RestConsumeApisApplication.java
+++ spring-17-REST-ConsumeApis/src/main/java/com/cydeo/Spring17RestConsumeApisApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients // can be placed at configuration classes
 public class Spring17RestConsumeApisApplication {
 
     public static void main(String[] args) {
