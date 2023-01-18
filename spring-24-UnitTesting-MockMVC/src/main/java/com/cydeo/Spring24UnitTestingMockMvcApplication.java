ee98d73f47c245dd935f4cfdbe32f8affaf56f11
DilaraAsl <dcavus@gmail.com>
2023-01-17 17:08:02 -0800

    add spring 24 module files

--- spring-24-UnitTesting-MockMVC/src/main/java/com/cydeo/Spring24UnitTestingMockMvcApplication.java
+++ spring-24-UnitTesting-MockMVC/src/main/java/com/cydeo/Spring24UnitTestingMockMvcApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring24UnitTestingMockMvcApplication {
@@ -9,5 +11,8 @@ public class Spring24UnitTestingMockMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(Spring24UnitTestingMockMvcApplication.class, args);
     }
-
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
 }
