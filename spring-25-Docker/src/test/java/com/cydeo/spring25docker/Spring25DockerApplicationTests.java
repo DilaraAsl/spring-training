f2a284f54f71fec27a4bf62c774cbfdc0f1b6751
DilaraAsl <dcavus@gmail.com>
2023-01-17 16:59:43 -0800

    adding new modules

---
+++ spring-25-Docker/src/test/java/com/cydeo/spring25docker/Spring25DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring25docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring25DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
